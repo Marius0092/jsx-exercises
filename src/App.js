865acf395e550c0e7a5e7d53416a6b0c62de95c9
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-09-29 01:01:36 +0200

    jsx-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { HelloWorld } from "./HelloWorld";
+import { Hello } from "./Hello";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
-                <HelloWorld />
+                <Hello />
             </div>
         )
     }
